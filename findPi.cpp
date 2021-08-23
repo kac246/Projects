f936f10adf922cda310270f3e2afadeceb9c10ab
Krystal Clarke <84393629+kac246@users.noreply.github.com>
2021-08-22 22:42:31 -0300

    Update findPi.cpp

--- findPi.cpp
+++ findPi.cpp
@@ -1,1 +1,22 @@
+/*
+Name of Program : findPi.cpp
+Purpose : Find PI to the Nth Digit 
+Description : Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
+Author : Krystal Clarke
+*/
 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double pi1 = M_PI;
+    int decimal_places =0;
+    cout << "Please enter a number of decimal places with the max decimal place being 50." << endl;
+    cin >> decimal_places;
+
+  cout << "The generated PI value is " <<setprecision(decimal_places)<< M_PI << endl;
+}
